96b5f6c13a2954571a7d9dacff1b5f2fc2cddaf8
valerie schraauwers <valerieschraauwers@gmail.com>
2018-11-20 18:33:09 +0100

    yelp mvp

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.1'
-
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
 # Use sqlite3 as the database for Active Record
@@ -22,6 +21,13 @@ gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
+gem 'faker'
+
+gem 'bootstrap-sass'
+gem 'font-awesome-sass', '~> 5.0.9'
+gem 'simple_form'
+gem 'autoprefixer-rails'
+gem 'jquery-rails' # Add this line if you use Rails 5.1 or higher
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,32 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+categories = %w(chinese italian japanese french belgian)
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying all restaurants"
+Restaurant.destroy_all
+
+10.times do
+  restaurant = Restaurant.create!(
+    name: Faker::HeyArnold.location,
+    address: Faker::Address.street_address,
+    category: categories.sample)
+end
+
+puts "Created restaurants"
+Review.destroy_all
+puts "destorying all reviews"
+
+5.times do
+  review = Review.create!(
+    content: Faker::Lorem.sentence(3),
+    rating: rand(1..5),
+    restaurant_id: rand(1..10)
+    )
+end
+
+
+puts "Created all reviews"
+puts "done!"
